8e5972dbc37e6fe3faf50ed498ac859c16bf4d42
fciker2603-cmd <fciker2603@gmail.com>
2026-09-10 07:16:32 +0000

    commit add --all&& git commit -m "Initial comit"

---
+++ aspirest/src/main/java/aspirest/AspirestApplication.java
@@ -0,0 +1,13 @@
+package aspirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspirestApplication.class, args);
+	}
+
+}
---
+++ aspirest/src/test/java/aspirest/AspirestApplicationTests.java
@@ -0,0 +1,13 @@
+package aspirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

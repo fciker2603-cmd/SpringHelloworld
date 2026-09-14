f18c331242caf5535a031d806177b03ee3c48706
fciker2603-cmd <fciker2603@gmail.com>
2026-09-14 08:24:35 +0000

    git commit -m "Reintentar crear el proyecto con las configuraciones y comprobar  el resultado"

--- .devcontainer/aspirest/src/main/java/com/example/aspirest/AspirestApplication.java
+++ .devcontainer/aspirest/src/main/java/com/example/aspirest/AspirestApplication.java
@@ -1,4 +1,4 @@
-package aspirest;
+package com.example.aspirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- .devcontainer/aspirest/src/test/java/com/example/aspirest/AspirestApplicationTests.java
+++ .devcontainer/aspirest/src/test/java/com/example/aspirest/AspirestApplicationTests.java
@@ -1,4 +1,4 @@
-package aspirest;
+package com.example.aspirest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
